2abf5f997ccc47cffc99957b18857f845eaf71ee
Scott Crowder <scrowder@gmail.com>
2024-02-14 13:59:12 -0600

    create book model

---
+++ MyBooks/Book.swift
@@ -0,0 +1,53 @@
+//
+//  Book.swift
+//  MyBooks
+//
+//  Created by SCOTT CROWDER on 2/14/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Book {
+    var title: String
+    var author: String
+    var dateAdded: Date
+    var dateStarted: Date
+    var dateCompleted: Date
+    var summary: String
+    var rating: Int?
+    var status: Status
+    
+    init(title: String, author: String, dateAdded: Date = Date(), dateStarted: Date = Date.distantPast, dateCompleted: Date = Date.distantPast, summary: String = "", rating: Int? = nil, status: Status = .onShelf) {
+        self.title = title
+        self.author = author
+        self.dateAdded = dateAdded
+        self.dateStarted = dateStarted
+        self.dateCompleted = dateCompleted
+        self.summary = summary
+        self.rating = rating
+        self.status = status
+    }
+    
+    #if DEBUG
+    static var SampleData: Book = Book(title: "Sample Book", author: "Random Writer")
+    #endif
+}
+
+enum Status: Int, Codable, Identifiable, CaseIterable {
+    case onShelf, inProgress, completed
+    
+    var id: Self {self}
+    
+    var desc: String {
+        switch self {
+        case .onShelf:
+            return "On Shelf"
+        case .inProgress:
+            return "In Progress"
+        case .completed:
+            return "Completed"
+        }
+    }
+}
