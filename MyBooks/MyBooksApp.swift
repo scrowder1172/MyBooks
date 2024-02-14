1b3f217e3ef8ac7275ee9d2980b3233b79ec8674
Scott Crowder <scrowder@gmail.com>
2024-02-14 16:57:48 -0600

    update SwiftData definition

--- MyBooks/MyBooksApp.swift
+++ MyBooks/MyBooksApp.swift
@@ -10,17 +10,36 @@ import SwiftUI
 
 @main
 struct MyBooksApp: App {
+    
+    let container: ModelContainer
+    
     var body: some Scene {
         WindowGroup {
             BookListView()
         }
-        .modelContainer(for: Book.self)
+//        .modelContainer(for: Book.self)
+        .modelContainer(container)
     }
     
     init() {
+        let schema: Schema = Schema([Book.self])
+        let config: ModelConfiguration = ModelConfiguration("MyBooks", schema: schema)
+        do {
+            container = try ModelContainer(for: schema, configurations: config)
+        } catch {
+            fatalError("Could not configure the container")
+        }
+//        let config: ModelConfiguration = ModelConfiguration(url: URL.documentsDirectory.appending(path: "MyBooks.store"))
+//        do {
+//            container = try ModelContainer(for: Book.self, configurations: config)
+//        } catch {
+//            fatalError("Could not configure the container")
+//        }
+        
         #if DEBUG
         /// capture database file location for initial development and debugging
         print("Database file: \(URL.applicationSupportDirectory.path(percentEncoded: false))")
+//        print(URL.documentsDirectory.path())
         #endif
     }
 }
